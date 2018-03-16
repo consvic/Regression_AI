e5c939efef1d130a3be285f8f6dae91e56bb8921
cocoy <cocoy_shuffle@me.com>
2018-03-16 17:24:05 -0600

    final touches

--- main_log.py
+++ main_log.py
@@ -4,20 +4,26 @@ from sklearn.model_selection import train_test_split
 # import some and load data to play with
 digits = datasets.load_digits()
 
-# X_train, X_test, y_train, y_test = train_test_split(wine.data, wine.target, )
+# Split and shuffle the data
 X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.20, random_state=42)
 # offset = int(X.shape[0] * 0.8)
 # X_train, y_train = X[:offset], y[:offset]
 # X_test, y_test = X[offset:], y[offset:]
 
 # Create the model
-logreg = linear_model.LogisticRegression(C=1)
+logreg = linear_model.LogisticRegression(C=0.01)
 
 # We fit the data
 logreg.fit(X_train, y_train)
 
+# Print the results
 print("Training set score: {:.3f}".format(logreg.score(X_train, y_train)))
 print("Test set score: {:.3f}".format(logreg.score(X_test, y_test)))
 
 digits_y_pred = logreg.predict(X_test)
-print("Predicted values vs output values: {:.3f}".format(abs(y_test - digits_y_pred)))
+'''print("Test set y:")
+print(y_test)
+print("Prediction set y:")
+print(digits_y_pred)
+print("Absolute value of Y test and Y prediction")
+print(abs(y_test - digits_y_pred))'''
--- main_ridge.py
+++ main_ridge.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
 
 # Load the california housing dataset
 california_housing = datasets.fetch_california_housing()
